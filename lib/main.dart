866c0fe06756d0f7c7ffb48801b1340d7fdfe8c9
Peyno <75258235+Peyno@users.noreply.github.com>
2025-11-02 21:25:13 +0100

    finishing Video 56

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(255, 101, 54, 182), 
+              Color.fromARGB(255, 163, 131, 219), 
+            ],
+          )
+        ),
+        child: const StartScreen(),)),
+    ),
+  );
+}
